9799cd87d8909b17e6030588568d056fd96a1a89
ChrisBonheur <bonheurmafoundou@gmail.com>
2020-04-01 23:45:22 +0100

    I've set a class to test all about update article view

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -2,8 +2,8 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from django.shortcuts import reverse
 
-from store.models import Article
-from store.tests import get_user, create_article, USERNAME, PASSWORD
+from store.models import Article, Category
+from store.tests import get_user, create_article, category, USERNAME, PASSWORD
 
 class MyArticlesTestCase(TestCase):
     @classmethod
@@ -38,4 +38,54 @@ class MyArticlesTestCase(TestCase):
              [repr(article) for article in unavailable_articles], msg="Unavailable \
                  articles of user miss in context")
         
+class UpdateArticleTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = get_user()# create an user
+        category()#create category
+        article = create_article()# create an article
 
+    def setUp(self):
+        self.user = User.objects.get(username=USERNAME)
+        self.article = Article.objects.last()
+        self.client.login(username=USERNAME, password=PASSWORD)
+        self.response = self.client.get(reverse('dashboard:update', args=(self.article.id,)))
+
+    def test_acces_page_update(self):
+        response = self.response
+        self.assertEqual(response.status_code, 200)
+        
+    def test_context_contains(self):
+        article = self.article
+        response = self.response
+        category = Category.objects.get(name="Informatique")
+        #test context contain article
+        self.assertEqual(article, response.context['article'], msg="Context not contain article")
+        #test context contain categories
+        self.assertQuerysetEqual(response.context['categories'], [repr(category)])
+    
+    def test_save_update(self):
+        article = self.article
+        data = {
+            "article_name": "Salade",
+            "details": article.description,
+            "category": article.category,
+            "status": article.status,
+            "article_number": 4,
+            "price_init": 12500,
+            "town": article.town,
+            "district": article.district,
+            "delivery": True
+        }
+        
+        self.client.login(username=USERNAME, password=PASSWORD)
+        response = self.client.post(reverse('dashboard:update', args=(article.id,)), data)
+        article_updated = Article.objects.get(name="Salade")
+        #compare old data and new data like article.name
+        self.assertEqual(article_updated.name, data['article_name'], \
+            msg="Warning! Data haven't updtated")
+        #test redirection
+        self.assertRedirects(response, reverse('dashboard:my_articles'))
+    
+        
+        
