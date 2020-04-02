d254ed4dca4960a5809fb4d9856f8c833db198a9
ChrisBonheur <bonheurmafoundou@gmail.com>
2020-04-02 14:22:43 +0100

    Initializing of test class for view m_yarticles

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from store.models import Article
+from store.tests import get_user, create_article
+
+class MyArticlesTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = get_user() #create user
+        article = create_article() #create article 
+    
+
