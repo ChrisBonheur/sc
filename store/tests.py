f86d5bdce036bd1262a26788709c78377ca386d8
ChrisBonheur <bonheurmafoundou@gmail.com>
2020-12-30 06:59:46 +0100

    comment the precedent tests and adding for new class test with test_home method

--- store/tests.py
+++ store/tests.py
@@ -4,74 +4,83 @@ from django.contrib.auth.models import User
 
 from .models import Article, Category
 
-class HomePageTestCase(TestCase):
-    def test_home_page(self):
+class StoreTest(TestCase):
+    def test_home(self):
+        #Verify if saved articles is showed to home page
         response = self.client.get(reverse('home'))
         
+        print(response)
         self.assertEqual(response.status_code, 200)
+
+
+# class HomePageTestCase(TestCase):
+#     def test_home_page(self):
+#         response = self.client.get(reverse('home'))
+        
+#         self.assertEqual(response.status_code, 200)
         
 
-class DetailPageTestCase(TestCase):
+# class DetailPageTestCase(TestCase):
     
-    def setUp(self):
-        informatique = Category.objects.create(name='Informatique')
-        self.category = Category.objects.get(name='Informatique')
+#     def setUp(self):
+#         informatique = Category.objects.create(name='Informatique')
+#         self.category = Category.objects.get(name='Informatique')
         
-        bonheur = User.objects.create(
-            username='bonheur',
-            email='bonheur@gmail.com',
-            password='12345687'
-            )
-        self.user = User.objects.get(username='bonheur')
-        ordinateur = Article.objects.create(
-            name="Ordinateur portable",
-            description="Test description",
-            price_init=2500,
-            price_ttc=2700,
-            number=2,
-            town='Pointe-Noire',
-            district='Siafoumou',
-            status='Bon état',
-            category=self.category,
-            user=self.user
-        )
+#         bonheur = User.objects.create(
+#             username='bonheur',
+#             email='bonheur@gmail.com',
+#             password='12345687'
+#             )
+#         self.user = User.objects.get(username='bonheur')
+#         ordinateur = Article.objects.create(
+#             name="Ordinateur portable",
+#             description="Test description",
+#             price_init=2500,
+#             price_ttc=2700,
+#             number=2,
+#             town='Pointe-Noire',
+#             district='Siafoumou',
+#             status='Bon état',
+#             category=self.category,
+#             user=self.user
+#         )
         
-        self.article = Article.objects.get(name='Ordinateur portable')
+#         self.article = Article.objects.get(name='Ordinateur portable')
         
-    def test_detail_page_return_200(self):
-        response = self.client.get(reverse('store:detail', args=(self.article.id,)))
+#     def test_detail_page_return_200(self):
+#         response = self.client.get(reverse('store:detail', args=(self.article.id,)))
         
-        self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.status_code, 200)
     
-    # def test_detail_page_return_400(self):
-    #     article_id = self.article.id + 1 
-    #     response = self.client.get(reverse('store:detail', args=(article_id,)))
+#     # def test_detail_page_return_400(self):
+#     #     article_id = self.article.id + 1 
+#     #     response = self.client.get(reverse('store:detail', args=(article_id,)))
         
-    #     self.assertEqual(response.status_code, 400)
+#     #     self.assertEqual(response.status_code, 400)
 
 
-class SellPageTestCase(DetailPageTestCase):
-    def test_sell_page_return_200(self):
-        response = self.client.get(reverse('store:sell'))
+# class SellPageTestCase(DetailPageTestCase):
+#     def test_sell_page_return_200(self):
+#         response = self.client.get(reverse('store:sell'))
         
-        self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.status_code, 200)
     
-    def test_sell_page_save_article(self):
-        articles_count = Article.objects.count()
-        response = self.client.post(reverse('store:sell'), {
-            "article_name": "Ordinateur portable",
-            "details": "Test description",
-            "price_init": 2500,
-            "price_ttc": 2700,
-            "article_number": 2,
-            "town": 'Pointe-Noire',
-            "district": 'Siafoumou',
-            "status":'Bon état',
-            "category": self.category,
-            "user_id": self.user.id,
-            "count_click": 4
-        })
+#     def test_sell_page_save_article(self):
+#         articles_count = Article.objects.count()
+#         response = self.client.post(reverse('store:sell'), {
+#             "article_name": "Ordinateur portable",
+#             "details": "Test description",
+#             "price_init": 2500,
+#             "price_ttc": 2700,
+#             "article_number": 2,
+#             "town": 'Pointe-Noire',
+#             "district": 'Siafoumou',
+#             "status":'Bon état',
+#             "category": self.category,
+#             "user_id": self.user.id,
+#             "count_click": 4
+#         })
 
-        new_count_article = Article.objects.count()
+#         new_count_article = Article.objects.count()
         
-        self.assertEqual(new_count_article, articles_count + 1)
+#         self.assertEqual(new_count_article, articles_count + 1)
