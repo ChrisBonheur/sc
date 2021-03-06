d7e5fb5b2d118a5d2d9fb37865a6e4eab52da2a0
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-03-06 18:21:38 +0100

    Adding for 3 globals functions  to get user, category, create article and remove thems from setups

--- store/tests.py
+++ store/tests.py
@@ -8,39 +8,59 @@ from .models import Article, Category
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+def get_user():
+    try:
+        user = User.objects.get(username="bonheur")
+    except:
+        pass
+    else:
+        user.delete()
+    finally:
+        user = User.objects.create(
+            username='bonheur',
+            email='bonheur@gmail.com',
+            password='12345687'
+        )
+    return user
+
+def category():
+    try:
+        category = Category.objects.get(name='Informatique')
+    except:
+        pass
+    else:
+        category.delete()
+    finally:
+        category = Category.objects.create(name='Informatique') 
+    return category
+
+def create_article():
+    article = Article.objects.create(
+        name="Ordinateur portable",
+        description="Test description",
+        price_init=2500,
+        price_ttc=2700,
+        number=2,
+        town='Pointe-Noire',
+        district='Siafoumou',
+        status='Bon état',
+        category=category(),
+        user=get_user(),
+        image_min=File(open(f'{BASE_DIR}/store/static/store/img_test/pic5.png', 'rb'))
+    )
+    
+    return article
+
 class HomePageTestCase(TestCase):
     def test_home_page(self):
-        response = self.client.get(reverse('home'))
-        
+        response = self.client.get(reverse('home'))    
         self.assertEqual(response.status_code, 200)
         
 class DetailPageTestCase(TestCase):
     
     def setUp(self):
-        informatique = Category.objects.create(name='Informatique')
-        self.category = Category.objects.get(name='Informatique')
-        
-        bonheur = User.objects.create(
-            username='bonheur',
-            email='bonheur@gmail.com',
-            password='12345687'
-            )
-        self.user = User.objects.get(username='bonheur')
-        ordinateur = Article.objects.create(
-            name="Ordinateur portable",
-            description="Test description",
-            price_init=2500,
-            price_ttc=2700,
-            number=2,
-            town='Pointe-Noire',
-            district='Siafoumou',
-            status='Bon état',
-            category=self.category,
-            user=self.user,
-            image_min=File(open(f'{BASE_DIR}/store/static/store/img_test/pic5.png', 'rb'))
-        )
-        
-        self.article = Article.objects.get(name='Ordinateur portable')
+        # self.user = get_user()
+        self.article = create_article()
         
     def test_detail_page_return_200(self):
         response = self.client.get(reverse('store:detail', args=(self.article.id,)))
@@ -55,9 +75,11 @@ class DetailPageTestCase(TestCase):
 
 
 class SellPageTestCase(DetailPageTestCase):
+    def setUp(self):
+        self.user = get_user()
+        
     def test_sell_page_return_200(self):
         response = self.client.get(reverse('store:sell'))
-        
         self.assertEqual(response.status_code, 200)
     
     def test_sell_page_save_article(self):
@@ -71,7 +93,7 @@ class SellPageTestCase(DetailPageTestCase):
             "town": 'Pointe-Noire',
             "district": 'Siafoumou',
             "status":'Bon état',
-            "category": self.category,
+            "category": category(),
             "user_id": self.user.id,
             "count_click": 4
         })
