6d948bbae7ca092339a15c01c206ee7f8e4b6bf5
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-03-10 18:31:11 +0100

    init base for testing communication.new_message and create for first test to access to new_message page

--- communication/tests.py
+++ communication/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
-# Create your tests here.
+from .models import Message, Talk
+
+class NewMessageTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user1 = User.objects.create_user(username="chris", password="123", email="chris@gmail.com")
+        user2 = User.objects.create_user(username="dan", password="1234", email="dan@gmail.com")
+        talk = Talk.objects.create(user1_id=user1.id, user2_id=user2.id)
+    
+    def setUp(self):
+        self.user1 = User.objects.get(username="chris")
+        self.user2 = User.objects.get(username="dan")
+        self.client.login(username="chris", password="123")
+        self.logged_get = lambda url_name, data: self.client.get(reverse(url_name), data)
+        self.logged_post = lambda url_name, data: self.client.post(reverse(url_name), data)
+        self.talk = Talk.objects.get(user1_id=self.user1.id, user2_id=self.user2.id)
+        
+    def test_acces_to_page_for_new_message(self):
+        talk_id = self.talk.id
+        response = self.logged_get('communication:new_msg', {"talk_id": talk_id})
+        
+        self.assertEqual(response.status_code, 200)
