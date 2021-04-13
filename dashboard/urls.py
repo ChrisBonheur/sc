d924f733e5c7dc80ac120bfc1de70ada838dfd58
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-04-13 16:53:40 +0100

    remove old links about invoices and replace it by new links

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,14 +6,14 @@ urlpatterns = [
     re_path(r'^$', views.my_articles, name='my_articles'),
     re_path(r'^update/(?P<article_id>[0-9]+)/$', views.update_article, name='update'),
     re_path(r'^delete/', views.delete_article, name='delete'),
-    re_path(r'^create_invoice/', views.create_invoice, name="create_invoice"),
-    re_path(r'^received_invoice/', views.received_invoice, name="received_invoice"),
-    re_path(r'^delete_invoice/(?P<invoice_id>[0-9]+)/$', views.delete_invoice, name="delete_invoice"),
+    # re_path(r'^create_invoice/', views.create_invoice, name="create_invoice"),
+    # re_path(r'^received_invoice/', views.received_invoice, name="received_invoice"),
+    # re_path(r'^delete_invoice/(?P<invoice_id>[0-9]+)/$', views.delete_invoice, name="delete_invoice"),
     re_path(r'^selled_list/$', views.selled_article_list, name='selled_list'),
     re_path(r'^bought_list/$', views.bought_article_list, name='bought_list'),
     
     #ajout provisoire de url
-    re_path(r'^invoices/', views.invoices, name="invoices"),
+    re_path(r'^factures/[a-z.ç]*', views.invoices, name="invoices"),
     re_path(r'^commandes/[a-zç]*$', views.orders, name="orders"),
 ]
 
