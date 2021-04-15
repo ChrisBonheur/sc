057ca05f4d68f77bb263d40389dbd86d0a82e227
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-04-15 15:55:00 +0100

    creating of setUpTestData method to contain models that need to be initialize one time  and get it in setUp

--- dashboard/tests/tests_models.py
+++ dashboard/tests/tests_models.py
@@ -2,24 +2,33 @@ from django.test import TestCase
 from django.contrib.auth.   models import User
 
 from dashboard.models import Invoice, Order
+from store.models import Article
 from store.tests.tests_views import create_article
  
 class SignalsTestCase(TestCase):
     """Test all signals about store app"""
-    def setUp(self):
-        self.user = User.objects.create_user(username="exo", password="123456",\
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username="exo", password="123456",\
             email="exo@gmail.com")
+        article = create_article("radio")
+        order = Order.objects.create(
+            user=user,
+            article=article,
+        )        
+    def setUp(self):
+        self.user = User.objects.get(username="exo")
+        self.article = Article.objects.get(name="radio")
+        self.order = Order.objects.get(article=self.article)
+            
     def test_upload_manage(self):
         """test if order's attribut manage become True
         when invoice is created for this order
         """
         user = self.user
-        article = create_article()
+        article = self.article
         #create order
-        order = Order.objects.create(
-            user=user,
-            article=article,
-        )
+        order = self.order
         #create invoice
         invoice = Invoice.objects.create(order=order)
         order = Order.objects.get(article=article)
@@ -30,5 +39,8 @@ class SignalsTestCase(TestCase):
         """test that article.available is False when seller or article.user
         decline an order's article
         """
+        #get an order
+        #make a request http with "decliner-la-commande" inside and order_id
+        #assert(order.article.available, False)
         pass
         
