bc6819647908e8c7dd51c2a0d8d0577e0b14dab7
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-04-15 15:27:32 +0100

    remove the older invoice path

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,9 +6,6 @@ urlpatterns = [
     re_path(r'^$', views.my_articles, name='my_articles'),
     re_path(r'^update/(?P<article_id>[0-9]+)/$', views.update_article, name='update'),
     re_path(r'^delete/', views.delete_article, name='delete'),
-    # re_path(r'^create_invoice/', views.create_invoice, name="create_invoice"),
-    # re_path(r'^received_invoice/', views.received_invoice, name="received_invoice"),
-    # re_path(r'^delete_invoice/(?P<invoice_id>[0-9]+)/$', views.delete_invoice, name="delete_invoice"),
     re_path(r'^selled_list/$', views.selled_article_list, name='selled_list'),
     re_path(r'^bought_list/$', views.bought_article_list, name='bought_list'),
     
