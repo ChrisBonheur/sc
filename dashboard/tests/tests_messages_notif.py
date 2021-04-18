6334abb6341b41e130957dc66acb7627840ff0f4
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-04-18 08:52:05 +0100

    adding for article_update_success variable

--- dashboard/tests/tests_messages_notif.py
+++ dashboard/tests/tests_messages_notif.py
@@ -2,15 +2,33 @@ from django.test import TestCase
 from django.shortcuts import reverse
 
 from store.tests.tests_views import create_article, PASSWORD
-from dashboard.messages_info import article_delete_success
+from dashboard.messages_info import article_delete_success, article_update_success
 
 class MessageNotifTestCase(TestCase):
+    def setUp(self):
+        self.article = create_article()
+        self.client.login(username=self.article.user, password=PASSWORD)
+        
     def test_success_delete_article(self):
-        """test message info showed in page  after deleting of an
+        """test message info success deleting is added after deleting of an
         article"""
-        article = create_article()
-        self.client.login(username=article.user, password=PASSWORD)
+        article = self.article
         response = self.client.get(reverse('dashboard:delete'), {"article_id": article.id})
         response = self.client.get(reverse('dashboard:my_articles'))
         self.assertContains(response, article_delete_success(article))
+    
+    def test_success_update_article(self):
+        """test message info succes update is added after
+        update action"""
+        article = self.article
+        attributs = ["category", "status", "price_init", "town", "district",\
+             "delivery"]
+        data_article = {attr: getattr(article, attr) for attr in attributs}
+        data_article["article_name"] = article.name
+        data_article["article_number"] = article.number
+        data_article["details"] = article.description
+        self.client.post(reverse('dashboard:update', args=(article.id,)), data_article)
+        response = self.client.get(reverse('dashboard:my_articles'))
+        self.assertContains(response, article_update_success(article))
+        
         
