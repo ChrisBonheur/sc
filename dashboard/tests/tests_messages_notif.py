ed0b8372b9348cea29cf7d4f5c9b7dd1ceb3220b
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-04-20 16:14:29 +0100

    remove some explicit key to append it on attributs list and in test assert i replace argument of article_update_succes by data_article[name], because article return old article's name after update it and could not pass test

--- dashboard/tests/tests_messages_notif.py
+++ dashboard/tests/tests_messages_notif.py
@@ -1,19 +1,20 @@
 from django.test import TestCase
 from django.shortcuts import reverse
 
-from store.tests.tests_views import create_article, PASSWORD
+from store.tests.tests_views import create_article, PASSWORD, IMAGE, get_user
 from dashboard.messages_info import article_delete_success, article_update_success
 
 class MessageNotifTestCase(TestCase):
     def setUp(self):
-        self.article = create_article()
+        self.user = get_user('davel')
+        self.article = create_article("clavier", self.user)
         self.client.login(username=self.article.user, password=PASSWORD)
         
     def test_success_delete_article(self):
         """test message info success deleting is added after deleting of an
         article"""
         article = self.article
-        response = self.client.get(reverse('dashboard:delete'), {"article_id": article.id})
+        self.client.get(reverse('dashboard:delete'), {"article_id": article.id})
         response = self.client.get(reverse('dashboard:my_articles'))
         self.assertContains(response, article_delete_success(article))
     
@@ -21,14 +22,13 @@ class MessageNotifTestCase(TestCase):
         """test message info succes update is added after
         update action"""
         article = self.article
-        attributs = ["category", "status", "price_init", "town", "district",\
-             "delivery"]
+        attributs = ["status", "price_init", "town", "district",\
+             "delivery", "number", "description", "image_min"]
         data_article = {attr: getattr(article, attr) for attr in attributs}
-        data_article["name"] = article.name
-        data_article["number"] = article.number
-        data_article["description"] = article.description
+        data_article["name"] = "clavier azerty"
+        data_article["category"] = article.category.id
         self.client.post(reverse('dashboard:update', args=(article.id,)), data_article)
         response = self.client.get(reverse('dashboard:my_articles'))
-        self.assertContains(response, article_update_success(article))
+        self.assertContains(response, article_update_success(data_article["name"]))
         
         
