415e95a0022a1abd3d68563b9bee1320f0dc9a49
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-05-20 12:06:43 +0100

    rename path to french

--- store/urls.py
+++ store/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     re_path(r'^(?P<article_id>[0-9]+)/$', views.detail, name='detail'),
     re_path(r'^poster-un-article/', views.create_article, name="create_article"),
     re_path(r'^modifier-article/(?P<article_id>[0-9]+)/$', views.update, name='update'),
-    re_path(r'^favourite/', views.favourite, name="favourite"),
+    re_path(r'^mes-articles-favoris/$', views.favourite, name="favourite"),
     re_path(r'^search/', views.search, name="search"),
 ]
 
