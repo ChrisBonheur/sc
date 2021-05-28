6060d9f1911e782f20860ee194d24f38996c4517
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-05-28 19:19:48 +0100

    Adding for block confirmation script and move jumbotron script to new file store/main.js coz this script is activate only in store app

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,27 +1,48 @@
-/*jumbotron diaporama by bnhr*/
-i = 1; //begin by jumbotron at position 2, the first is alredy load before setinterval
-const diaporama = ()=>{
-    let elt = $('.jumbotron')[i];
-    $('.jumbotron').fadeOut(0);
-    $(elt).css({
-    "background": ` gray url(${$('.jumbotron img')[i].getAttribute("src")}) no-repeat center center`,
-    "background-size": "contain",
-    }).fadeIn(2000);
-
-    i++;
-    if (i > ($('.jumbotron').length - 1)){
-        i = 0;
-    }
+/*Componnent dialog block*/
+const showDialogConfirm = (title, link, text=$('.dialog_confirm .card-text').text()) => {
+    $('.dialog_confirm .card-title').text(title);
+    $('.dialog_confirm .card-text').text(text);
+    $('.dialog_confirm .btn-danger').attr("href", link);
+    //show dialog_confirm
+    $('.dialog_confirm').removeClass("d-none");
 }
 
-const main = () => {
-    //show the first jumbotron before the set interval begin in 5000 ms
-    $('.jumbotron:first').css({
-        "background": `gray url(${$('.jumbotron img')[0].getAttribute("src")}) no-repeat center center`,
-        "background-size": "contain", 
-    }).fadeIn();
-    
-    setInterval(diaporama, 5000);
+const dialogEvent = () => {
+    $('.dialog_confirm_before').on("click", (e) => {
+        e.preventDefault();
+        try {
+            title = e.target.children[0].textContent;
+            link = e.target.href
+            showDialogConfirm(title, link)
+        } catch (error) {
+            console.error(`Set a hidden span with class "dialog_confirm_before" 
+            that contain title of dialog confirmation in currentTarget
+            like => <span class="dialog_confirm_before">Deleting article</span>`);
+        }
+
+        $('.dialog_confirm .btn-primary').on('click', () => {
+            //if cancel, hide dialog confirm
+            $('.dialog_confirm').addClass("d-none");
+        });
+    });
 }
 
-main();
+const main = () => { 
+    //==============================================
+    //script for app store
+    const linkAppStore = '/static/store/js/main.js';
+    $.getScript(linkAppStore)
+    .done((script, textStatus) => {
+        //launch main of app store
+        mainStore();
+    })
+    .fail((jqxhr, settings, exception) =>{
+        console.error(`Can't find ${linkAppStore}`);
+    });
+
+    dialogEvent();
+}
+
+$(() => {
+    main();
+})
