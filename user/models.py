5e8370cfce1e3e286b90e85f13d3ba0e7f6d95c9
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-07-01 16:23:20 +0100

    Edit and add of somme columns in user model

--- user/models.py
+++ user/models.py
@@ -4,12 +4,21 @@ from PIL import Image
 
 from store.utils import edit_image_before_save
 
+class Gender(models.Model):
+    sexe = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.sexe
+
 class Profil(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     avatar = models.ImageField(upload_to='avatars/', null=True)
-    gender = models.CharField(max_length=100, null=True)
-    contact = models.IntegerField(null=True)
-    mobile_money = models.IntegerField(null=True)
+    gender = models.ForeignKey(Gender, on_delete=models.SET_NULL, null=True)
+    contact_mtn = models.CharField(null=True, max_length=20)
+    contact_airtel = models.CharField(null=True, max_length=20)
+    whatsap_number = models.CharField(null=True, max_length=20)
+    airtel_money = models.CharField(null=True, max_length=20)
+    mtn_money = models.CharField(null=True, max_length=20)
     
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
