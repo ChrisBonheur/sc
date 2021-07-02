d3366df3396919e6bc816e1b5d04aeb29c7e7175
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-07-02 04:27:31 +0100

    comment old line code for convert time in human view

--- store/templatetags/store_extras.py
+++ store/templatetags/store_extras.py
@@ -5,35 +5,35 @@ from store.models import Favourite
 
 register = template.Library()
 
-@register.filter
-def calcul_date(date_time):
-    """This function calcul and return time since article has benn added"""
-    date_calcul = timezone.now() - date_time
-    if date_calcul.days <= 0:
-        if date_calcul.seconds < 60:
-            return f'{date_calcul.seconds}s'
-        elif date_calcul.seconds >= 60 and date_calcul.seconds < 3600:
-            minute = int(date_calcul.seconds / 60)
-            return f'{minute}min'
-        else:
-            hour = int(date_calcul.seconds / 3600)
-            return f'{hour}hr'
-    else:
-        if date_calcul.days < 30:
-            days = date_calcul.days
-            indication = 'jr'
-            if days > 1:
-                indication = 'jrs'
-            return f'{date_calcul.days}{indication}'
-        if date_calcul.days >= 30 and date_calcul.days < 360:
-            month = int(date_calcul.days / 30)
-            return f'{month}mois'
-        else:
-            year = int(date_calcul.days / 360)
-            indication = 'an'
-            if year > 1:
-                indication = 'ans'
-            return f'{year}{indication}'
+# @register.filter
+# def calcul_date(date_time):
+#     """This function calcul and return time since article has benn added"""
+#     date_calcul = timezone.now() - date_time
+#     if date_calcul.days <= 0:
+#         if date_calcul.seconds < 60:
+#             return f'{date_calcul.seconds}s'
+#         elif date_calcul.seconds >= 60 and date_calcul.seconds < 3600:
+#             minute = int(date_calcul.seconds / 60)
+#             return f'{minute}min'
+#         else:
+#             hour = int(date_calcul.seconds / 3600)
+#             return f'{hour}hr'
+#     else:
+#         if date_calcul.days < 30:
+#             days = date_calcul.days
+#             indication = 'jr'
+#             if days > 1:
+#                 indication = 'jrs'
+#             return f'{date_calcul.days}{indication}'
+#         if date_calcul.days >= 30 and date_calcul.days < 360:
+#             month = int(date_calcul.days / 30)
+#             return f'{month}mois'
+#         else:
+#             year = int(date_calcul.days / 360)
+#             indication = 'an'
+#             if year > 1:
+#                 indication = 'ans'
+#             return f'{year}{indication}'
         
 @register.filter
 def is_favourite(user, article):
