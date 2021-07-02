fac85d7fc927a4fdca9ec4528664f3e872c343a1
ChrisBonheur <bonheurmafoundou@gmail.com>
2021-07-02 04:50:26 +0100

    Add link attribute in model Class

--- communication/models.py
+++ communication/models.py
@@ -29,6 +29,7 @@ class ChatMessage(Message):
         return self.content
     
 class NotifMessage(Message):
+    link = models.CharField(max_length=20, null=True)
     def __str__(self):
         return self.content
 
